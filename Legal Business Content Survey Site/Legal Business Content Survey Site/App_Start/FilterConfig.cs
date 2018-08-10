cae6816d4cf0dc00f04d684f5681c911318dbf76
Jason Barnwell (CELA) <jabarnwe@microsoft.com>
2018-08-09 19:26:00 -0700

    Adding Application Insights telemetry.

--- Legal Business Content Survey Site/Legal Business Content Survey Site/App_Start/FilterConfig.cs
+++ Legal Business Content Survey Site/Legal Business Content Survey Site/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Legal_Business_Content_Survey_Site
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
